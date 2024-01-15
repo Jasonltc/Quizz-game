c83b19b1665118f14044ad2d0f9a80dacd4471e5
Jasonltc <jasonltc_14@hotmail.com>
2024-01-15 11:18:45 -0800

    quiz section

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@ const popupInfo = document.querySelector(".popup-info");
 const continueBtn = document.querySelector(".continue-btn");
 const exitBtn = document.querySelector(".exit-btn");
 const main = document.querySelector(".main");
+const quizSection = document.querySelector(".quiz-section");
+// const home = document.querySelector(".home");
 
 startBtn.onclick = () => {
   popupInfo.classList.add("show");
@@ -13,3 +15,14 @@ exitBtn.onclick = () => {
   popupInfo.classList.remove("show");
   main.classList.remove("active");
 };
+
+continueBtn.onclick = () => {
+  quizSection.classList.add("active");
+  popupInfo.classList.remove("show");
+  main.classList.remove("active");
+};
+
+// window.addEventListener("click", (bae) => {
+//   let x = bae.target;
+//   console.log(x);
+// });
