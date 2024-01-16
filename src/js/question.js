68e522ccf0f032e61fce51445aa3125315918000
Jasonltc <jasonltc_14@hotmail.com>
2024-01-16 03:41:17 -0800

    pre-complete

---
+++ src/js/question.js
@@ -0,0 +1,59 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "C. Hyper Text Markup Language",
+    options: [
+      "A. Hyper Text Markup Language 1",
+      "B. Hyper Text Markup Language 1",
+      "C. Hyper Text Markup Language",
+      "D. Hyper Text Markup Language 1",
+    ],
+  },
+  {
+    numb: 2,
+    question: "What does HTML stand for?",
+    answer: "B. Hyper Text Markup Language",
+    options: [
+      "A. Hyper Text Markup Language 2",
+      "B. Hyper Text Markup Language",
+      "C. Hyper Text Markup Language 2",
+      "D. Hyper Text Markup Language 2",
+    ],
+  },
+  {
+    numb: 3,
+    question: "What does HTML stand for?",
+    answer: "B. Hyper Text Markup Language",
+    options: [
+      "A. Hyper Text Markup Language 3",
+      "B. Hyper Text Markup Language",
+      "C. Hyper Text Markup Language 3",
+      "D. Hyper Text Markup Language 3",
+    ],
+  },
+  {
+    numb: 4,
+    question: "What does HTML stand for?",
+    answer: "D. Hyper Text Markup Language",
+    options: [
+      "A. Hyper Text Markup Language 4",
+      "B. Hyper Text Markup Language 4",
+      "C. Hyper Text Markup Language 4",
+      "D. Hyper Text Markup Language",
+    ],
+  },
+  {
+    numb: 5,
+    question: "What does HTML stand for?",
+    answer: "A. Hyper Text Markup Language",
+    options: [
+      "A. Hyper Text Markup Language 5",
+      "B. Hyper Text Markup Language",
+      "C. Hyper Text Markup Language 5",
+      "D. Hyper Text Markup Language 5",
+    ],
+  },
+];
+
+export default questions;
