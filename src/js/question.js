b9cc9a79dd3bd52bf99594202d66df7e833efb29
Jasonltc <jasonltc_14@hotmail.com>
2024-01-17 09:45:07 -0800

    Finish the Game

--- src/js/question.js
+++ src/js/question.js
@@ -1,58 +1,105 @@
 let questions = [
   {
     numb: 1,
-    question: "What does HTML stand for?",
-    answer: "C. Hyper Text Markup Language",
+    question: "What is JavaScript?",
+    answer:
+      "A. JavaScript is a scripting language used to make the website interactive",
     options: [
-      "A. Hyper Text Markup Language 1",
-      "B. Hyper Text Markup Language 1",
-      "C. Hyper Text Markup Language",
-      "D. Hyper Text Markup Language 1",
+      "A. JavaScript is a scripting language used to make the website interactive",
+      "B. JavaScript is an assembly language used to make the website interactive",
+      "C. JavaScript is a compiled language used to make the website interactive",
+      "D. None of the mentioned",
     ],
   },
   {
     numb: 2,
-    question: "What does HTML stand for?",
-    answer: "B. Hyper Text Markup Language",
+    question: "Which of the following is correct about JavaScript?",
+    answer: "A. JavaScript is an Object-Based language",
     options: [
-      "A. Hyper Text Markup Language 2",
-      "B. Hyper Text Markup Language",
-      "C. Hyper Text Markup Language 2",
-      "D. Hyper Text Markup Language 2",
+      "A. JavaScript is an Object-Based language",
+      "B. JavaScript is Assembly-language",
+      "C. JavaScript is an Object-Oriented language",
+      "D. JavaScript is a High-level language",
     ],
   },
   {
     numb: 3,
-    question: "What does HTML stand for?",
-    answer: "B. Hyper Text Markup Language",
+    question:
+      "Arrays in JavaScript are defined by which of the following statements?",
+    answer: "A. It is an ordered list of values",
     options: [
-      "A. Hyper Text Markup Language 3",
-      "B. Hyper Text Markup Language",
-      "C. Hyper Text Markup Language 3",
-      "D. Hyper Text Markup Language 3",
+      "A. It is an ordered list of values",
+      "B. It is an ordered list of objects",
+      "C. It is an ordered list of string",
+      "D. It is an ordered list of functions",
     ],
   },
   {
     numb: 4,
-    question: "What does HTML stand for?",
-    answer: "D. Hyper Text Markup Language",
-    options: [
-      "A. Hyper Text Markup Language 4",
-      "B. Hyper Text Markup Language 4",
-      "C. Hyper Text Markup Language 4",
-      "D. Hyper Text Markup Language",
-    ],
+    question:
+      "Which of the following object is the main entry point to all client-side JavaScript features and APIs?",
+    answer: "B. Window",
+    options: ["A. Position", "B. Window", "C. Standard", "D. Location"],
   },
   {
     numb: 5,
-    question: "What does HTML stand for?",
-    answer: "A. Hyper Text Markup Language",
+    question: "What is the basic difference between JavaScript and Java?",
+    answer:
+      "B. Functions are values, and there is no hard distinction between methods and fields",
     options: [
-      "A. Hyper Text Markup Language 5",
-      "B. Hyper Text Markup Language",
-      "C. Hyper Text Markup Language 5",
-      "D. Hyper Text Markup Language 5",
+      "A. Functions are considered as fields",
+      "B. Functions are values, and there is no hard distinction between methods and fields",
+      "C. Variables are specific",
+      "D. There is no difference",
     ],
+  },
+  {
+    numb: 6,
+    question: "Why JavaScript Engine is needed?",
+    answer: "C. Interpreting the JavaScript",
+    options: [
+      "A. Both Compiling & Interpreting the JavaScript",
+      "B. Parsing the javascript",
+      "C. Interpreting the JavaScript",
+      "D. Compiling the JavaScript",
+    ],
+  },
+  {
+    numb: 7,
+    question: "What is CSS?",
+    answer: "D. All of the mentioned",
+    options: [
+      "A. CSS is a style sheet language",
+      "B. CSS is designed to separate the presentation and content, including layout, colors, and fonts",
+      "C. CSS is the language used to style the HTML documents",
+      "D. All of the mentioned",
+    ],
+  },
+  {
+    numb: 8,
+    question:
+      "Which of the following CSS framework is used to create a responsive design?",
+    answer: "D. bootstrap",
+    options: ["A. django", "B. rails", "C. larawell", "D. bootstrap"],
+  },
+  {
+    numb: 9,
+    question:
+      "Which of the following CSS property is used to make the text bold?",
+    answer: "B. font-weight: bold",
+    options: [
+      "A. text-decoration: bold",
+      "B. font-weight: bold",
+      "C. font-style: bold",
+      "D. ext-align: bold",
+    ],
+  },
+  {
+    numb: 10,
+    question:
+      " Which of the following CSS style property is used to specify an italic text?",
+    answer: "C. font-style",
+    options: ["A. style", "B. font", "C. font-style", "D. @font-face"],
   },
 ];
 
